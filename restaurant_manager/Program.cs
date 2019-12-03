e0f93f51600cc5aa34c13caa4737d28fff50729e
tanhop <tanhopdo2000@gmail.com>
2019-12-03 13:32:20 +0700

    them text

--- restaurant_manager/Program.cs
+++ restaurant_manager/Program.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace restaurant_manager
 {
     class Program
     {
-        static void Main(string[] args)
-        {   
+            static void Main(string[] args)
+        {
             Menu.nhap();
             Menu.xuat();
             Console.ReadKey();
         }
+       
     }
 }
